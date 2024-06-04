e156ad8e5c7aaa3202e4679ccab62dd4dcd12f26
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 17:19:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DescendingOrder(10);
+            static void DescendingOrder(int n) 
+            {
+                if (n == 0)
+                {
+                    return;
+                }
+                else 
+                {
+                    Console.Write(n + " ");
+                    DescendingOrder(n - 1);
+                }
+
+            }
+        }
+    }
+}
